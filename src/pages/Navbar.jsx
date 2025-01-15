d41fe59137cef5d79785420783fbad375bbf5ac4
someshwarsholkar <someshwarsholkar22@gmail.com>
2025-01-15 16:59:19 +0530

    location design update

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -11,12 +11,9 @@ const Navbar = () => {
     return (
         <nav className="bg-white text-black shadow-lg shadow-slate-300">
             <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-                {/* Logo */}
                 <div className="text-2xl font-bold">
                     <a href="#home">Solar Cleaner</a>
                 </div>
-
-                {/* Desktop Menu */}
                 <ul className="hidden md:flex space-x-6">
                     <li>
                         <a href="#home" className="hover:text-yellow-500">Home</a>
@@ -31,32 +28,27 @@ const Navbar = () => {
                         <a href="#contact" className="hover:text-yellow-500">Contact</a>
                     </li>
                 </ul>
-
-                {/* Mobile Menu Button */}
                 <button
                     className="md:hidden focus:outline-none"
-                    onClick={toggleMenu}
-                >
+                    onClick={toggleMenu}>
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         className="h-6 w-6"
                         fill="none"
                         viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
+                        stroke="currentColor">
                         <path
                             strokeLinecap="round"
                             strokeLinejoin="round"
                             strokeWidth={2}
-                            d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
-                        />
+                            d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"} />
                     </svg>
                 </button>
             </div>
             {isOpen && <div className={`md:hidden bg-transparent  text-center space-y-2 px-4 py-4 ${isOpen ? 'block' : 'hidden'} transition-all duration-300 ease-in-out`}>
                 <ul className={`space-y-2 ${isOpen ? 'opacity-100' : 'opacity-0'} transition-opacity duration-300`}>
                     <li onClick={e => setIsOpen(false)} className="font-semibold">
-                        <Link to="/" className="block hover:text-yellow-500">Home</Link>
+                        <a href="#home" className="block hover:text-yellow-500">Home</a>
                     </li>
                     <li onClick={e => setIsOpen(false)} className="font-semibold">
                         <a href="#service" className="block hover:text-yellow-500">Services</a>
@@ -70,7 +62,7 @@ const Navbar = () => {
                 </ul>
             </div>}
         </nav >
-    );
-};
+    )
+}
 
-export default Navbar;
+export default Navbar
